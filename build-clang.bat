d1e3af83b654f21eeeaef9863343058dec95dc8b
ChyZma <konrad0816@gmail.com>
2022-02-13 19:17:51 +0100

    adds imshader to build.sh and build-clang.bat

--- build-clang.bat
+++ build-clang.bat
@@ -9,17 +9,20 @@ set CommonOptions=-std=c++20 -g -Oi -O3
 set CommonDefines=-DDEVELOPER=1 -DPLATFORM_WIN32=1 -DWIN32_LEAN_AND_MEAN -DNOMINMAX
 set Warnings=-Werror -Wno-multichar
 set IncludeDirs=-Isrc/ -I%VULKAN_SDK%/Include/
-set LinkerOptions=-L%VULKAN_SDK%/Lib/ -lvulkan-1 -lkernel32 -luser32 
+set LinkerOptions=-L%VULKAN_SDK%/Lib/ -lvulkan-1 -lkernel32 -luser32
 set Options=%CommonOptions% %CommonDefines% %Warnings% %IncludeDirs% %LinkerOptions%
 
-clang %Options% "src/Win32_Main.cpp" "src/Game.cpp" -obuild/blokker.exe 
+clang %Options% "src/Win32_Main.cpp" "src/Game.cpp" -obuild/blokker.exe
 
 IF NOT EXIST shader mkdir shader
 cd shader
 del /q *
 cd ..
 
-set ShaderCompilerOptions=--target-env=vulkan1.2 -std=450core -I "src/shader/" -O 
+set ShaderCompilerOptions=--target-env=vulkan1.2 -std=450core -I "./src/shader/" -O
 
 glslc %ShaderCompilerOptions% -fshader-stage=vert -o shader/shader.vs -DVERTEX_SHADER=1 "src/shader/shader.glsl"
 glslc %ShaderCompilerOptions% -fshader-stage=frag -o shader/shader.fs -DFRAGMENT_SHADER=1 "src/shader/shader.glsl"
+
+glslc %ShaderCompilerOptions% -fshader-stage=vert -o shader/imshader.vs -DVERTEX_SHADER=1 "src/shader/imshader.glsl"
+glslc %ShaderCompilerOptions% -fshader-stage=frag -o shader/imshader.fs -DFRAGMENT_SHADER=1 "src/shader/imshader.glsl"
